6156f5cdf907774136ffc647d7e4abecaf12e3ab
luxiaobo <luxb@getui.com>
2018-12-10 16:43:29 +0800

    跟新

---
+++ src/main/java/java8/test.java
@@ -0,0 +1,12 @@
+package java8;
+
+/**
+ * @author xiaobo.lu
+ * @date ${date}
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
