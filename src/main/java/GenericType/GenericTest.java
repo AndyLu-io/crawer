fbb010fdd7568b2619bb0c9e62dae720374872fa
luxiaobo <2404198216@qq.com>
2019-06-12 16:16:47 +0800

    增加泛型测试实现

---
+++ src/main/java/GenericType/GenericTest.java
@@ -0,0 +1,11 @@
+package GenericType;
+
+/**
+ * @author luxiaobo
+ * 泛型测试
+ */
+public class GenericTest {
+    public static void main(String[] args) {
+        System.out.printf("hello world");
+    }
+}
